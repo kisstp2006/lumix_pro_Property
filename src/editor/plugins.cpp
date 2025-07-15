d2e7e14b31ee3faab94dad05c5c75ea2efcafd90
Kiss Tibor Peter <kisstp2006@gmail.com>
2025-07-15 22:54:55 +0200

    Implemented basic keyframe drawing logic -TIGames

--- src/editor/plugins.cpp
+++ src/editor/plugins.cpp
@@ -4,9 +4,25 @@
 #include "editor/world_editor.h"
 
 #include "imgui/imgui.h"
-
+#include <vector> // Include necessary header for std::vector
 
 using namespace Lumix;
+
+/*
+// we dont need these for now, but we can implement them later if needed
+enum class PropertyType
+{
+	FLOAT,
+	VEC3,
+	VEC4,
+	COLOR,
+	QUATERNION,
+	BOOL
+};
+*/
+struct Keyframe {
+    float time; // Define the structure for a keyframe
+};
 
 struct EditorPlugin : StudioApp::GUIPlugin {
 	EditorPlugin(StudioApp& app) : m_app(app) {}
@@ -15,7 +31,17 @@ struct EditorPlugin : StudioApp::GUIPlugin {
 		ImGui::SetNextWindowSize(ImVec2(200, 200), ImGuiCond_FirstUseEver);
 		if (ImGui::Begin("Pro Property")) {
 			ImGui::TextUnformatted("Hello world");
-			ImGui::DragFloat("Some value", &m_some_value);
+			ImDrawList* draw_list = ImGui::GetWindowDrawList();
+			ImVec2 canvas_pos = ImGui::GetCursorScreenPos();
+			ImVec2 canvas_size = ImGui::GetContentRegionAvail();
+
+			float total_time = 1.0f; // Assuming 1.0f represents the total duration of the timeline
+
+			// Keyframe drawing
+			for (auto& keyframe : keyframes) {
+				float x = canvas_pos.x + (keyframe.time / total_time) * canvas_size.x;
+				draw_list->AddCircleFilled(ImVec2(x, canvas_pos.y + 20), 5, IM_COL32(255, 0, 0, 255));
+			}
 		}
 		ImGui::End();
 	}
@@ -24,6 +50,11 @@ struct EditorPlugin : StudioApp::GUIPlugin {
 
 	StudioApp& m_app;
 	float m_some_value = 0;
+
+	// Add a member variable to store keyframes
+	std::vector<Keyframe> keyframes = {
+		{0.2f}, {0.5f}, {0.8f} // Example keyframes with time values
+	};
 };
 
 
